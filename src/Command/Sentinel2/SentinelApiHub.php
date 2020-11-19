5c04fd20faaa5c99c40f77c8e666e81ba3bb0a78
Alex Dontsov <dcorpsoftware@gmail.com>
2020-11-19 22:27:14 +0700

    add docker

--- src/Command/Sentinel2/SentinelApiHub.php
+++ src/Command/Sentinel2/SentinelApiHub.php
@@ -5,6 +5,7 @@ namespace App\Command\Sentinel2;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpClient\HttpClient;
 
 /**
  * Class SentinelApiHub
@@ -12,6 +13,8 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class SentinelApiHub extends Command
 {
+
+
     /**
      * @var string
      */
@@ -24,6 +27,47 @@ class SentinelApiHub extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        return Command::SUCCESS;
+        $this->getData();
     }
+
+    private function getData()
+    {
+
+        $client = HttpClient::create();
+        $response = $client->request(
+            'GET',
+            'https://scihub.copernicus.eu/dhus/odata/v1/Products(\'22e7af63-07ad-4076-8541-f6655388dc5e\')/$value', [
+                'auth_basic' => ['rumato', '123qweR$'],
+                'headers' => [
+                    'Content-Type' => 'text/plain',
+                ],
+            ]
+        );
+
+        $statusCode = $response->getStatusCode();
+
+
+        if (200 !== $response->getStatusCode()) {
+            throw new \Exception('...');
+        }
+
+// get the response content in chunks and save them in a file
+// response chunks implement Symfony\Contracts\HttpClient\ChunkInterface
+        $fileHandler = fopen('/home/alex/DCORP/Symfony/sibwater/ubuntu.zip', 'w');
+        foreach ($client->stream($response) as $chunk) {
+            fwrite($fileHandler, $chunk->getContent());
+        }
+
+
+//        // $statusCode = 200
+//        $contentType = $response->getHeaders()['content-type'][0];
+//        // $contentType = 'application/json'
+//        $content = $response->getContent();
+//        // $content = '{"id":521583, "name":"symfony-docs", ...}'
+//        $content = $response->toArray();
+//        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+//        $z = $content;
+        return 1;
+    }
+
 }
--- src/Utils/SentinelApiHub/DataDownloader.php
+++ src/Utils/SentinelApiHub/DataDownloader.php
@@ -20,14 +20,32 @@ class DataDownloader
      */
     public function __construct(HttpClientInterface $client, string $apiKey)
     {
-
+        $this->client = $client;
+        $this->endpoint = sprintf('https://%s.rest.akismet.com/1.1/comment-check', $apiKey);
     }
 
     /**
      * @param array $context
+     * @return int
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
      */
     public function getData(array $context)
     {
         $response = $this->client->request('POST', $this->endpoint, []);
+
+        $headers = $response->getHeaders();
+        if ('discard' === ($headers['x-akismet-pro-tip'][0] ?? '')) {
+            return 2;
+        }
+        $content = $response->getContent();
+        if (isset($headers['x-akismet-debug-help'][0])) {
+            throw new \RuntimeException(sprintf('Unable to check for spam: %s
+                   (%s).', $content, $headers['x-akismet-debug-help'][0]));
+        }
+
+        return 'true' === $content ? 1 : 0;
     }
 }
