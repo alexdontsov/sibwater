53235c3983504f4beadeb554ed416e1fc9650b44
Alex Dontsov <dcorpsoftware@gmail.com>
2020-12-15 10:33:31 +0700

    add redis

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Log\Logger;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,7 +33,6 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-//        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         return [
             MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
 
