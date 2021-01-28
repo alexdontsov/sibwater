ddf664c4249805174c5db59b9579a44e51ba30f2
Alex Dontsov <dcorpsoftware@gmail.com>
2021-01-28 11:40:27 +0700

    add template includes

--- application/src/Controller/PageController.php
+++ application/src/Controller/PageController.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace App\Controller
+namespace App\Controller;
 
-use App\Repository\ConferenceRepository;
+use App\Repository\RegionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 use Twig\Error\LoaderError;
 use Twig\Error\RuntimeError;
 use Twig\Error\SyntaxError;
@@ -15,14 +17,36 @@ use Twig\Error\SyntaxError;
 class PageController extends AbstractController
 {
     /**
+     * @var Environment
+     */
+    private Environment $twig;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+
+    /**
+     * ConferenceController constructor.
+     * @param Environment $twig
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(Environment $twig, EntityManagerInterface $entityManager)
+    {
+        $this->twig = $twig;
+        $this->entityManager = $entityManager;
+    }
+
+
+    /**
      * @Route("/", name="homepage")
-     * @param ConferenceRepository $conferenceRepository
+     * @param RegionRepository $conferenceRepository
      * @return Response
      * @throws LoaderError
      * @throws RuntimeError
      * @throws SyntaxError
      */
-    public function index(ConferenceRepository $conferenceRepository)
+    public function index(RegionRepository $conferenceRepository)
     {
         return new Response($this->twig->render('pages/index.html.twig', [
             'conferences' => $conferenceRepository->findAll(),
